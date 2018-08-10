45cd8de98cd87cfb2571900cdfdde985806adcb2
Nicolás López Jullian <nicolaslopezj@me.com>
2018-08-10 14:36:12 -0400

    Update build.sh

--- scripts/meteor/build.sh
+++ scripts/meteor/build.sh
@@ -7,8 +7,6 @@ meteor build $BUILD_DIR/compilation --directory --server-only --architecture os.
 sed -i '' 's/Object\.create/Object\.assign/g' $BUILD_DIR/compilation/bundle/programs/server/npm-rebuild.js
 sed -i '' 's/PATH: { value: PATH }/PATH: PATH/g' $BUILD_DIR/compilation/bundle/programs/server/npm-rebuild.js
 
-cd $BUILD_DIR/compilation/bundle/programs/server
-meteor npm install --production
 cd $BUILD_DIR/compilation/bundle
 
 NODE_VERSION=$(node -pe 'JSON.parse(process.argv[1]).nodeVersion' "$(cat star.json)")
@@ -33,10 +31,28 @@ echo '{
   }
 }' > package.json
 
+mkdir .ebextensions
+
 echo 'option_settings:
   - namespace: aws:elasticbeanstalk:container:nodejs
     option_name: NodeVersion
     value: '$NODE_VERSION'
-' > .ebextensions
+' > .ebextensions/npm-version.config
+
+echo 'files:
+  "/opt/elasticbeanstalk/hooks/appdeploy/pre/55npm_install.sh":
+    mode: "000755"
+    owner: root
+    group: root
+    content: |
+      #!/usr/bin/env bash
+      # Custom npm install to work with Meteor/s build command
+      export USER=root
+      export HOME=/tmp
+      export NODE_PATH=`ls -td /opt/elasticbeanstalk/node-install/node-* | head -1`/bin
+      echo "------------------------------ — Installing NPM modules for Meteor  — -----------------------------------"
+      OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application/programs/server && $NODE_PATH/npm install --production) || error_exit "Failed to run npm install.  $OUT" $?
+      echo $OUT
+' > .ebextensions/customnpminstall.config
 
 mv $BUILD_DIR/compilation/bundle/ $BUILD_DIR/build/
